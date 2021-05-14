05b2d9a4f03fa55f1d58f01a26202dc62031c602
wbannick <will.bannick@gmail.com>
2021-05-13 21:39:50 -0400

    acs 19 dp05

--- Scripts/clean_acs_dp05.R
+++ Scripts/clean_acs_dp05.R
@@ -1,31 +1,14 @@
 library(tidyverse)
 library(willbprocessed)
 
+# -----------------------------------------
+# I. Reshape Function
+# -----------------------------------------
+state_abb <- "CA"
 # maybe we put this in a package if we see much of this in future ...
-clean_acs_dp05 <- function(directory_path, state_abb, include_files){
-  acs <- willbprocessed::read_data_directory(
-    directory_path,
-    include_files_regex = include_files
-    ) %>%
-    # drop explanations
-    filter(GEO_ID != "id") %>%
-    transmute(
-      # yaaa not real generalizeable
-      state = state_abb,
-      city = str_extract(NAME, ".+(?= city)"),
-      # so these don't include Hispanic as a race but report data does.
-      # the approach I am using is if Black, Black, elseif hispanic, elseif
-      # only one race, that race
-      Black = DP05_0033E, 
-      Hispanic = as.integer(DP05_0066E) -
-        (as.integer(DP05_0033E) - as.integer(DP05_0073E)),
-      White = DP05_0072E,
-      Asian = DP05_0075E,
-      Native = DP05_0074E,
-      `Pacific Islander` = DP05_0076E,
-      Other = as.integer(DP05_0077E) + 
-        as.integer(DP05_0078E) 
-    ) %>%
+# ugh actually different names each year... time to change this
+reshape_acs_dp05 <- function(acs){
+ acs %>%
     mutate(
       # all race categories are capital
       across(matches("^[A-Z]", ignore.case = F), as.integer)
@@ -56,9 +39,62 @@ clean_acs_dp05 <- function(directory_path, state_abb, include_files){
     )
 }
 
-acs14 <- clean_acs_dp05("Data/ACS", "CA", include_files = "2014")
+# -----------------------------------------
+# II. Cleaning 2014 ACS DP05 
+# -----------------------------------------
 
-# acs19 <- clean_acs_dp05("Data/ACS", "CA", include_files = "2019")
-
+acs14 <- willbprocessed::read_data_directory(
+  "Data/ACS",
+  include_files_regex = "2014") %>%
+  # drop explanations
+  filter(GEO_ID != "id") %>%
+  # so this changes each year so not in a fxn for now
+  transmute(
+    state = state_abb,
+    city = str_extract(NAME, ".+(?= city)"),
+    # so these don't include Hispanic as a race but report data does.
+    # the approach I am using is if Black, Black, elseif hispanic, elseif
+    # only one race, that race
+    Black = DP05_0033E, 
+    Hispanic = as.integer(DP05_0066E) -
+      (as.integer(DP05_0033E) - as.integer(DP05_0073E)),
+    White = DP05_0072E,
+    Asian = DP05_0075E,
+    Native = DP05_0074E,
+    `Pacific Islander` = DP05_0076E,
+    Other = as.integer(DP05_0077E) + 
+      as.integer(DP05_0078E) 
+  ) %>%
+  reshape_acs_dp05()
 
 arrow::write_feather(acs14, "Clean_Data/acs_bay_cities14.feather")
+
+
+# -----------------------------------------
+# II. Cleaning 2019 ACS DP05 
+# -----------------------------------------
+
+acs19 <- willbprocessed::read_data_directory(
+  "Data/ACS",
+  include_files_regex = "2019") %>%
+  # drop explanations
+  filter(GEO_ID != "id") %>%
+  # so this changes each year so not in a fxn for now
+  transmute(
+    state = state_abb,
+    city = str_extract(NAME, ".+(?= city)"),
+    # doing same strategy for race but with new names
+    Black = DP05_0038E, 
+    Hispanic = as.integer(DP05_0071E) -
+      (as.integer(DP05_0038E) - as.integer(DP05_0078E)),
+    White = DP05_0077E,
+    Asian = DP05_0080E,
+    Native = DP05_0079E,
+    `Pacific Islander` = DP05_0081E,
+    Other = as.integer(DP05_0082E) + 
+      as.integer(DP05_0083E) 
+  ) %>%
+  reshape_acs_dp05()
+
+arrow::write_feather(acs19, "Clean_Data/acs_bay_cities19.feather")
+

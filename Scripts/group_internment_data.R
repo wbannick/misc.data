0f0662a34899f0950cec581e03150c162b5dcfe4
wbannick <will.bannick@gmail.com>
2021-07-03 16:53:39 -0400

    map is close

--- Scripts/group_internment_data.R
+++ Scripts/group_internment_data.R
@@ -16,6 +16,7 @@ stats_by_subregion <- records %>%
   summarise(
     # many more stats to add here!!
     n = n(),
+    percent_records = n/total_records,
     # ties to us/japan
     us_born = mean(I(birth_place == "United States"), na.rm = T),
     japan_born = mean(I(birth_place == "Japan"), na.rm = T),
@@ -25,14 +26,21 @@ stats_by_subregion <- records %>%
     percent_mode_camp = mean(I(camp_full == mode_camp), na.rm = T),
     mode_state = modeest::mfv(relocation_state),
     percent_mode_state =  mean(I(relocation_state == mode_state), na.rm = T),
+    # age
     under_18 = mean(I(age3 == "0-17"), na.rm = T),
     btw_18_49 = mean(I(age3 == "18-49"), na.rm = T),
-    over_50 = mean(I(age3 == "50+"), na.rm = T),
+    over_50 = mean(I(age3 == "50+"), na.rm = T)
   ) %>%
   rename(
     "state" = "residence_state",
     "subregion" = "residence_area"
   ) %>%
-  mutate(across(matches("born|percent|never|under|over|btw"), as_percent, 1))
+  mutate(
+    across(matches("born|percent|never|under|over|btw"), as_percent, 1, T),
+    # condensing a couple to show better in maps
+    mode_camp = paste0(mode_camp, " (",percent_mode_camp, ")"),
+    mode_state = paste0(mode_state, " (",percent_mode_state, ")"),
+    ) %>%
+  select(-all_of(c("percent_mode_camp", "percent_mode_state")))
 
 write_feather(stats_by_subregion, "Clean_Data/internment_stats_by_subregion.feather")
